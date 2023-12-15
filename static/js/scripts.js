1b1343a78d323f7b31dc3f7b767fac60c2e1866c
il    iciuv <miguel.garcia.duch@gmail.com>
2023-12-15 18:34:42 +0100

    last

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', "teaching", "portfolio", "contact"]
+const section_names = ['home', 'publications', "teaching", "portfolio", "contact"]
 
 
 window.addEventListener('DOMContentLoaded', event => {
